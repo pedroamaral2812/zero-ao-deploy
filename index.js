242483afe129f4c5bb282a03d715e8e07d2de7ff
Pedro Amaral <pedropaulo@life.com.br>
2019-11-25 15:29:27 -0300

    initial commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const Koa = require('Koa');
+const Router = require('koa-router')
+
+const app = new Koa()
+const router = new Router()
+
+const index = ctx => {
+  ctx.body = 'Node.Js do zero ao Deploy'
+}
+
+
+const hello = ctx => {
+  const name = ctx.params.name || 'World'
+  ctx.body = `Hello ${name}`
+}
+
+router.get('/',index)
+router.get('/hello', hello)
+router.get('/hello/:name', hello)
+
+app.use(router.routes())
+
+const port = 3000
+
+app.listen(port)
+  .on('listening', () => console.log(`Listening on port ${port}`))
